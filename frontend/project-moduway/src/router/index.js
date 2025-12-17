7c7c17f0882e3b86309a68c1ff4565638d363558
DESKTOP-bd <han970112@gmail.com>
2025-12-18 00:18:49 +0900

    feat(mainpage): IndexPage 추가 - coursecard는 목업 데이터(DB 연동 후 추가 예정)

--- frontend/project-moduway/src/router/index.js
+++ frontend/project-moduway/src/router/index.js
@@ -1,8 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import IndexPage from '@/pages/IndexPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: IndexPage,
+    },
+    // 다른 라우트들은 작업하면서 추가
+  ],
 })
 
 export default router
